27afe1ae4bcb26ff0d312204dd22751a2aa234d9
Vishnu P <vishnu.p@landmarkgroup.in>
2025-07-09 23:06:22 +0530

    Updated the constant.js

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,4 +1,4 @@
 // API URLs
-// export const API_BASE_URL = 'http://localhost:8000/api';
 export const API_BASE_URL = 'https://villagemitai.zapto.org/api';
+export const API_BASE_URL_MEDIA = 'https://villagemitai.zapto.org';
 
