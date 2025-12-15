7ae22fcab812b085dfbcd4404a506daa95995a1d
Vishnu P <vishnu.p@landmarkgroup.in>
2025-12-15 23:16:31 +0530

    Disable CORS for all origins and specify allowed origin

--- Backend/restaurant/restaurant/settings.py
+++ Backend/restaurant/restaurant/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/backend-static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://villagemitai.in",
     "https://www.villagemitai.in",
